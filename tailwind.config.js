6e89919452e93c8d5528c8186d8aa0dec99112d5
Lina Choi <choilina16@gmail.com>
2022-09-04 15:53:17 -0400

    inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        red: '#d25d5d',
+        blk: '#000000',
+        wht: '#ffffff',
+        gry: '#5f6369',
+        drGry: '#6b6b6b',
+      },
+    },
+  },
+  plugins: [],
+};
