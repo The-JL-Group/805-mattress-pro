06cd626df645e71aa37e98ec5fad2dce4c6a103e
jimbolikesgithub <jamesedwards@Jamess-MacBook-Pro.local>
2022-09-07 11:40:46 -0400

    Made actual overflow fix to Info component which was causing overflow issue

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -19,7 +19,7 @@ function Info() {
   }, []);
 
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section className="bg-white dark:bg-gray-900 overflow-x-hidden">
       <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
         <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
